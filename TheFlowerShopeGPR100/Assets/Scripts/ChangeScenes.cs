aef5d727fa60edcef169627a3ed4970528014959
Isabella Beaudoin <isabella.beaudoin@mymail.champlain.edu>
2024-10-28 13:12:13 -0600

    Added store menu outline

--- TheFlowerShopeGPR100/Assets/Scripts/ChangeScenes.cs
+++ TheFlowerShopeGPR100/Assets/Scripts/ChangeScenes.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Unity.VisualScripting;
+using System.Security.Cryptography;
 
 public class ChangeScenes : MonoBehaviour
 {
     public GameObject optionsPanel;
+    public GameObject storePanel;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -54,5 +56,20 @@ public class ChangeScenes : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("Start Screen");
+    }
+
+    public void OpenPanel()
+    {
+        if(storePanel != null)
+        {
+            Animator animator = storePanel.GetComponent<Animator>();
+            if(animator != null)
+            {
+                bool isOpen = animator.GetBool("open");
+
+                animator.SetBool("open", !isOpen);
+            }
+
+        }
     }
 }
